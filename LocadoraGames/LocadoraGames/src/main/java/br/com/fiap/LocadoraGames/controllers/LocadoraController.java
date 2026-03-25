ba8a39c2c94309ec1014322d0dccc8ddb629826e
PietroWilhelm <pietrowilhelm@git.com>
2026-03-25 12:39:04 -0300

    bugfix: refatoração do codigo ajustando os nomes

--- LocadoraGames/LocadoraGames/src/main/java/br/com/fiap/LocadoraGames/controllers/LocadoraController.java
+++ LocadoraGames/LocadoraGames/src/main/java/br/com/fiap/LocadoraGames/controllers/LocadoraController.java
@@ -1,44 +1,52 @@
 package br.com.fiap.LocadoraGames.controllers;
 
-import br.com.fiap.LocadoraGames.models.Jogo;
+import br.com.fiap.LocadoraGames.models.Game;
 import br.com.fiap.LocadoraGames.services.LocadoraService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
+@Slf4j
 public class LocadoraController {
 
     @Autowired
     private LocadoraService service;
 
     @GetMapping
-    public List<Jogo> listAll(){
-        return service.getAllJogos();
+    public List<Game> listAll(){
+        return service.getAllgames();
     }
 
     @PostMapping
-    public ResponseEntity<Jogo> createJogo(@RequestBody Jogo jogo){ //binding
+    public ResponseEntity<Game> createGame(@RequestBody Game game){ //binding
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(service.addJogo(jogo));
+                .body(service.addGame(game));
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Jogo> getJogoById(@PathVariable Long id){
+    public ResponseEntity<Game> getJogoById(@PathVariable Long id){
         log.info("Obtendo dados do jogo {}", id);
-        return ResponseEntity.ok(service.getJogoById(id));
+        return ResponseEntity.ok(service.getGameById(id));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Void> deleteJogo(@PathVariable Long id){
         log.info("Deletando jogo com id {}", id );
-        service.deleteJogo(id);
+        service.deleteGame(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Jogo> updateJogo(@PathVariable Long id, @RequestBody Jogo jogo){
-        log.info("Atualizando o Jogo com id {} com os dados {}", id, jogo);
-        return ResponseEntity.ok( service.updateJogo(id, jogo) );
+    public ResponseEntity<Game> updateJogo(@PathVariable Long id, @RequestBody Game game){
+        log.info("Atualizando o Jogo com id {} com os dados {}", id, game);
+        return ResponseEntity.ok( service.updateGame(id, game) );
     }
 
-}
 }
