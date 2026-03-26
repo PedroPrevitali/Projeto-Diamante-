23ab097d176ba1b9299819055ee026df54b031dc
pietro.wilhelm <pietro.wilhelm@ot.com.br>
2026-03-26 15:00:43 -0300

    Bugifx: AJUSTANDO A ESTRUTA DE PASTAS POR FAVOR NUNCA MEXER NELA

--- LocadoraGames/LocadoraGames/src/main/java/br/com/fiap/LocadoraGames/Repository/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.LocadoraGames.Repository;
-
-import br.com.fiap.LocadoraGames.models.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Long>{
-}
--- LocadoraGames/LocadoraGames/src/main/java/br/com/fiap/LocadoraGames/Repository/LocacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.LocadoraGames.Repository;
-
-import br.com.fiap.LocadoraGames.models.Locacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocacaoRepository extends JpaRepository<Locacao, Long> {
-}
